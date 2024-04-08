8056b106859a25e275930a454ff3b0d6e8170db5
Ikemefuna Onubogu <ikemefunaonubogu55@gmail.com>
2024-04-08 23:47:58 +0100

    changed anonymous image URL

--- app/ui/CreateReviewForm/bottom-buttons.tsx
+++ app/ui/CreateReviewForm/bottom-buttons.tsx
@@ -16,7 +16,7 @@ export default function BottomButtons() {
         try {
             if (values?.anonymous  === true) {
                 values.reviewerName = 'Anonymous'
-                values.reviewerImageURL = 'https://th.bing.com/th/id/OIP.0TsJGYhWWOy_hBFOH0hX-gHaHa?rs=1&pid=ImgDetMain'
+                values.reviewerImageURL = '/default-profile.png'
             }
             console.log(values)
             const response = await storeReview(values)
